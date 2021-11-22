9a34d337182035d886f25c0444f656fb1ba60dca
wlgus <wlgus@DESKTOP-LEJGQ62>
2021-11-22 10:49:38 +0900

    커밋

--- src/co/sp/controller/ShopController.java
+++ src/co/sp/controller/ShopController.java
@@ -6,7 +6,6 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 
 import org.apache.ibatis.annotations.Delete;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
